0129c9792289bbc2e34d02e1f94a1abb066644ed
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-20 17:21:00 +0300

    0x01. React intro

---
+++ 0x01. React intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Notifications.css";
+
+function Notifications() {
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
+
+export default Notifications;
