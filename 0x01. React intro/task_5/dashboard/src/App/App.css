f96e27b4fb31cb466c2926adf367d1495351ae1f
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-20 17:25:03 +0300

    0x01. React intro

---
+++ 0x01. React intro/task_5/dashboard/src/App/App.css
@@ -0,0 +1,38 @@
+.App {
+  height: 100vh;
+  max-width: 100vw;
+  position: relative;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.App-header {
+  font-size: 1.4rem;
+  color: #e0354b;
+  display: flex;
+  align-items: center;
+  border-bottom: 3px solid #e0354b;
+  padding-top: 200px;
+}
+
+.App-header img {
+  width: 200px;
+  height: 200px;
+}
+
+.App-body {
+  font-size: 1rem;
+  padding: 2em;
+  border-bottom: 3px solid #e0354b;
+  height: 45%;
+}
+
+.App-footer {
+  font-size: 1rem;
+  padding: 1.2rem;
+  text-align: center;
+  font-style: italic;
+}
+
+form input {
+  margin: 10px;
+}
