ba7d4618b4ad52fb62d6212fc1db30f7304bfc2c
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-01-21 09:38:16 +0300

    0x04. React inline styling

---
+++ 0x04. React inline styling/task_0/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+const WithLogging = (WrappedComponent) => {
+  const getDisplayName = (WrappedComponent) => WrappedComponent.displayName || WrappedComponent.name || "Component";
+
+  WithLogging.displayName = `WithLogging(${getDisplayName})`;
+
+  return class extends Component {
+    componentDidMount() {
+      console.log(`Component ${getDisplayName(WrappedComponent)} is mounted`);
+    }
+
+    componentWillUnmount() {
+      console.log(`Component ${getDisplayName(WrappedComponent)} is going to unmount`);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+
+export default WithLogging;
