667416ca0e78d3353efe6aaafcda9c593fb06128
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-03-05 00:28:04 +0300

    0x04-React_inline_styling

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Login from './Login';
+import { shallow } from 'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+describe("testing the <Login /> component", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<Login />);
+  });
+
+  it("Login component renders without crashing", () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it("Login component renders 2 input tags", () => {
+    expect(wrapper.find("input")).toHaveLength(2);
+  });
+
+  it("Login component renders 2 label tags", () => {
+    expect(wrapper.find("label")).toHaveLength(2);
+  });
+});
