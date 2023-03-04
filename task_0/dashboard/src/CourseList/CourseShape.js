b152f9165dc3f987725c6cc56b8b09e3715876bb
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-03-04 23:05:03 +0300

    Delete task_0/dashboard directory

--- task_0/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const CourseShape = PropTypes.shape({
-  id: PropTypes.number,
-  name: PropTypes.string,
-  credit: PropTypes.number,
-});
-
-export default CourseShape;
--- task_0/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as notificationsData from '../../notifications.json';
-
-export const getAllNotificationsByUser = (userId) => {
-  return notificationsData.default
-    .filter((item) => item.author.id === userId)
-    .map(({ context }) => context);
-};
