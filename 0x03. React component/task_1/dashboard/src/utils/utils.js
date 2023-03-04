879f9057c953a9bd58f3848ecb035d8cf6b4ed5b
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-03-04 23:48:59 +0300

    Delete 0x03. React component directory

--- 0x03. React component/task_1/dashboard/src/utils/utils.js
+++
@@ -1,5 +0,0 @@
-export const getFullYear = () => new Date().getFullYear();
-
-export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
-
-export const getLatestNotification = () => "<strong>Urgent Requirement</strong> - complete by EOD";
