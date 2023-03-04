8d9226f04c19a22a6fca8b3138ba650de9a5eb55
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2023-03-05 00:23:48 +0300

    Delete 0x04. React inline styling directory

--- 0x04. React inline styling/README.md
+++
@@ -1,1 +0,0 @@
-# 0x04. React inline styling
--- 0x04. React inline styling/task_2/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-const CourseShape = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  credit: PropTypes.number.isRequired,
-});
-
-export default CourseShape;
